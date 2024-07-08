d54ca8f7bac112c6fdacde0f1ea886a5649067b9
abrahaamv <velizabrahaam@gmail.com>
2024-07-08 12:58:15 -0600

    Added Will editor

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import PDFComponent from './PDFComponent'
-import object_status from './maried_with_kids'
+import React, { useState, useEffect } from 'react';
+import ReactDOMServer from 'react-dom/server';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import Article from './Article';
+import object_status from './maried_with_kids';
+
+
+const WillEditor = ({ object_status, onSave }) => {
+  const [editorContent, setEditorContent] = useState('');
+
+  useEffect(() => {
+    try {
+      const articleHtml = ReactDOMServer.renderToString(
+        <Article props={{ data: { datas: object_status } }} />
+      );
+      setEditorContent(articleHtml);
+    } catch (error) {
+      console.error("Error rendering Article:", error);
+      setEditorContent("");
+    }
+  }, [object_status]);
+
+  const handleChange = (content) => {
+    setEditorContent(content);
+  };
+
+  const handleSave = () => {
+    onSave(editorContent);
+  };
+
+  return (
+    <div>
+      <ReactQuill
+        value={editorContent}
+        onChange={handleChange}
+        theme="snow"
+        modules={{
+          toolbar: [
+            ['bold', 'italic', 'underline'],
+            [{ list: 'ordered' }, { list: 'bullet' }],
+          ],
+        }}
+      />
+      <button onClick={handleSave}>Save Will</button>
+    </div>
+  );
+};
 
 function App() {
+  const handleSaveWill = (content) => {
+    var willDOM = { willDOM: content }
+    object_status.push(willDOM);
+    console.log("object_status:", object_status);
+  };
+
 
   return (
     <>
-      <PDFComponent datas={object_status} />
+      <WillEditor
+        object_status={object_status}
+        onSave={handleSaveWill}
+      />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
